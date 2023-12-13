7b8a6d903d36e3d2a009c7a8a86967226410fe87
Armin Kuburas <akuburass@gmail.com>
2023-12-13 15:07:42 +0200

    Slowly working on it. Planning it out for now. Not fully sure how to implement the child and parent functions yet especially since exec function ends functions and processes

--- pipex/pipex.h
+++ pipex/pipex.h
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_pipex.c                                         :+:      :+:    :+:   */
+/*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/13 12:54:37 by akuburas          #+#    #+#             */
-/*   Updated: 2023/12/13 15:02:08 by akuburas         ###   ########.fr       */
+/*   Created: 2023/12/13 15:03:05 by akuburas          #+#    #+#             */
+/*   Updated: 2023/12/13 15:05:24 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
-
-int	main(int argc, char *argv[], char **env)
-{
-	int		p_fd[2];
-	pid_t	pid;
-
-	if (argc != 5)
-	{
-		perror("pipex infile cmd cmd outfile");
-		exit(0);
-	}
-	if (pipe(p_fd) == -1)
-		exit(-1);
-	pid = fork();
-	if (pid == -1)
-		exit(-1);
-	if (pid == 0)
-		child(argv, p_fd, env);
-	else
-		parent(argv, p_fd, env);
-}
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+#endif
