91b2b657936dff851e629dc9c57fbedbf7b8d89e
Armin Kuburas <akuburass@gmail.com>
2023-12-15 15:54:23 +0200

    Started working on pipex properly now. Have a few ideas of where to go with it. Have to make sure I handle errors as well

--- pipex/ft_pipex.c
+++ pipex/ft_pipex.c
@@ -6,11 +6,47 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 12:54:37 by akuburas          #+#    #+#             */
-/*   Updated: 2023/12/13 15:02:08 by akuburas         ###   ########.fr       */
+/*   Updated: 2023/12/15 15:53:45 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+static void	ft_execute(char *function, char **env)
+{
+	char	**function_commands;
+	char	*path;
+
+	function_commands = ft_pipex_split(function);
+	path = ft_path_make(function_commands[0], env);
+	
+}
+
+static void	child(char **argv, int p_fd[], char **env)
+{
+	int	fd;
+
+	fd = open(argv[1], O_RDONLY, 0777);
+	if (fd == -1)
+		exit (-1);
+	dup2(fd, 0);
+	dup2(p_fd[1], 1);
+	close(p_fd[0]);
+	ft_execute(argv[2], env);
+}
+
+static void	parent(char **argv, int p_fd[], char **env)
+{
+	int	fd;
+
+	fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fd == -1)
+		exit (-1);
+	dup2(fd, 1);
+	dup2(p_fd[0], 0);
+	close(p_fd[1]);
+	ft_execute(argv[3], env);
+}
 
 int	main(int argc, char *argv[], char **env)
 {
@@ -19,7 +55,7 @@ int	main(int argc, char *argv[], char **env)
 
 	if (argc != 5)
 	{
-		perror("pipex infile cmd cmd outfile");
+		perror("./pipex file1 cmd cmd file2");
 		exit(0);
 	}
 	if (pipe(p_fd) == -1)
@@ -28,7 +64,7 @@ int	main(int argc, char *argv[], char **env)
 	if (pid == -1)
 		exit(-1);
 	if (pid == 0)
-		child(argv, p_fd, env);
+		child(argv[1], p_fd, env);
 	else
-		parent(argv, p_fd, env);
+		parent(argv[4], p_fd, env);
 }
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 15:03:05 by akuburas          #+#    #+#             */
-/*   Updated: 2023/12/13 15:05:24 by akuburas         ###   ########.fr       */
+/*   Updated: 2023/12/15 10:54:03 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,6 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <fcntl.h>
+
 #endif
