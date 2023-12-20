96b34a7d1d17a050b1cb98976f64719e58cac83b
Armin Kuburas <akuburass@gmail.com>
2023-12-20 14:50:04 +0200

    Started working on the rework slowly.

---
+++ second_try_pipex/ft_pipex.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pipex.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 14:44:21 by akuburas          #+#    #+#             */
+/*   Updated: 2023/12/20 14:49:46 by akuburas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+static void	ft_execute(char *function, char **env)
+{
+	char	**function_commands;
+	char	*path;
+
+	function_commands = ft_pipex_split(function);
+	path = ft_path_make(function_commands[0], env);
+	if (execve(path, function_commands, env) == -1)
+	{
+		ft_putstr_fd("pipex: command not found: ", 2);
+		ft_putendl_fd(function_commands[0], 2);
+		ft_free_substrings(function_commands);
+		free(path);
+		exit(0);
+	}
+}
+
+static void	child(char **argv, int p_fd[], char **env)
+{
+	int	fd;
+	fd = open_file(argv[1], 0);
+	dup2(fd, 0);
+	dup2(p_fd[1], 1);
+	close(p_fd[0]);
+	ft_execute(argv[2], env);
+}
+
+static void	parent(char **argv, int p_fd[], char **env)
+{
+	int	fd;
+
+	fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fd == -1)
+		exit (-1);
+	dup2(fd, 1);
+	dup2(p_fd[0], 0);
+	close(p_fd[1]);
+	ft_execute(argv[3], env);
+}
+
+int	main(int argc, char *argv[], char **env)
+{
+	int		p_fd[2];
+	pid_t	pid;
+	int		status;
+
+	if (argc != 5)
+		handle_exit(1);
+	if (pipe(p_fd) == -1)
+		exit(-1);
+	pid = fork();
+	if (pid == -1)
+		exit(-1);
+	if (pid == 0)
+		child(argv, p_fd, env);
+	else
+	{
+		parent(argv, p_fd, env);
+		if (waitpid(pid, &status, 0) == -1)
+			exit(EXIT_FAILURE);
+	}
+}
