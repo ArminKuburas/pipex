447cbb37d09fce86ce54e19aa799c1e5bb2c0e34
Armin Kuburas <akuburass@gmail.com>
2024-01-02 11:34:42 +0200

    Adding the newest form of the Makefile. Finished now

--- official_release/header_files/pipex_bonus.h
+++ official_release/header_files/pipex_bonus.h
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 11:23:32 by akuburas          #+#    #+#             */
-/*   Updated: 2024/01/02 11:24:06 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/01/02 11:30:34 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,5 @@
 void	exit_handler(int n_exit);
 int		handle_file(char *file, int in_or_out);
 void	here_doc_handler(char **argv, int argc, int *fd_out);
-
 
 #endif
