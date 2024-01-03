5242771d38423c9944a923bf994510cf96d9246e
Armin Kuburas <akuburass@gmail.com>
2024-01-03 17:23:19 +0200

    Fixed up the test file a bit

--- experiments/test.sh
+++ experiments/test.sh
@@ -10,8 +10,8 @@ echo "And another line of content for fun" >> infile
 tests=(
   "infile 'ls -l' 'wc -l' outfile1"
   "file_does_not_exist 'ls -l' 'sleep 5' outfile2"
-  "file_does_not_exist 'sleep 10' 'echo hello' outfile 3"
-  "file_does_not_exist 'echo hello' 'sleep 5'
+  "file_does_not_exist 'sleep 10' 'echo hello' outfile3"
+  "file_does_not_exist 'echo hello' 'sleep 5' outfile4"
 )
 for test in "${tests[@]}"; do
   eval "./pipex $test"
